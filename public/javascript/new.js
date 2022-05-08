cfbeaf81e153cbacdf2b6fa098468890e803b6a1
Candace Robbins <canrob0522@gmail.com>
2022-05-08 10:27:15 -0500

    Added the POST for the new blog site

--- public/javascript/new.js
+++ public/javascript/new.js
@@ -4,13 +4,22 @@ const newFormHandler = async function(event) {
     const title = document.querySelector('input[name="post-title"]').value;
     const body = document.querySelector('textarea[name="post-body"]').value;
   
-    await fetch(`/api/post`, {
+    if (title && body) {
+    const response = await fetch(`/api/post`, {
          // Create the functionality to help create the buttons for your website.
-
+      method: "POST",
+      body: JSON.stringify({ title, body }),
+      headers: {
+        "Content-Type": "aplication/json",
+      },
     });
-  
+    if (response.ok) {
     document.location.replace('/dashboard');
-  };
+  } else {
+    alert('Failed to create project');
+  }
+}
+};
   
   document
     .querySelector('#new-post-form')
