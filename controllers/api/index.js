ad92e61f33f405d3e4b5837c2d472be78d5d5b27
Candace Robbins <canrob0522@gmail.com>
2022-05-08 15:38:25 -0500

    updated controller api index

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,13 +3,15 @@ const { route } = require("../../../.Main/controllers/api");
 const router = require("express").Router();
 
 // Finish the required pathing for these variables
-// const userRoutes = ;
+const userRoutes = require("./userRoutes");
 
-// const postRoutes = ;
+const postRoutes = require("./postRoutes");
 
-// const commentRoutes = ;
+const commentRoutes = require("./commentRoutes");
 
 router.use("/user", userRoutes);
-// write the rest of the router.use routes
+// write the rest of the router.use routes]
+router.use("/post", postRoutes);
+router.use("/comments", commentRoutes);
 
 module.exports = router;
