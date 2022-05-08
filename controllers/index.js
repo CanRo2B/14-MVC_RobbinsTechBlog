e492ce6e35cc073f5a307296ef661afabfca3f91
Candace Robbins <canrob0522@gmail.com>
2022-05-07 23:43:02 -0500

    tried to edit controller index.js

--- controllers/index.js
+++ controllers/index.js
@@ -2,13 +2,15 @@ const router = require("express").Router();
 
 // finish the required routes
 // const apiRoutes =
-const apiRoutes = require();
+const apiRoutes = require("./api");
 // const homeRoutes =
-const homeRoutes = require()
+const homeRoutes = require("./homeRoutes")
 // const dashboardRoutes =
-const dashboardRoutes = require();
+const dashboardRoutes = require("./dashboardRoutes");
 
 // router.use("/", homeRoutes);
+router.use("/", homeRoutes);
 // finish the other needed routes with router.use()
+router.use("/api", apiRoutes);
 
 module.exports = router;
