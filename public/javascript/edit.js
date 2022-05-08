63722a90a0ef697e075a511c3e55e5dcfde4db28
Candace Robbins <canrob0522@gmail.com>
2022-05-07 23:15:58 -0500

    adding edits to handlebars

--- public/javascript/edit.js
+++ public/javascript/edit.js
@@ -8,15 +8,24 @@ const editFormHandler = async function(event) {
 
   await fetch(`/api/post/${postId}`, {
        // Create the functionality to help create the buttons for your website.
-
+      method: "PUT",
+      body: JSON.stringify({
+        title,
+        body
+      }),
+      headers: {
+        "Content-Type": "application/json"
+      }
   });
 
   document.location.replace('/dashboard');
 };
 
 const deleteClickHandler = async function() {
-  await     // Create the functionality to help create the buttons for your website.
-
+       // Create the functionality to help create the buttons for your website.
+       await fetch (`/api/post/S{postId}`, {
+         method: "DELETE"
+       });
 
   document.location.replace('/dashboard');
 };
