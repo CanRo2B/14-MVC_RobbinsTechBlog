a22d9cfd35a9e2e4c3b49cca5918fe9c14c3b18f
Candace Robbins <canrob0522@gmail.com>
2022-05-08 14:03:07 -0500

    updated models

--- models/Comment.js
+++ models/Comment.js
@@ -5,6 +5,32 @@ class Comment extends Model {}
 
 Comment.init(
     // finish this Comment Model
-);
+ {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    post_id: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      references: {
+          model: "post",
+          key: "id",
+      },
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+          model: "user",
+          key: "id",
+      },
+    },
+    body: {
+      type: DataTypes.STRING,
+    },
+});
 
 module.exports = Comment;
--- models/Post.js
+++ models/Post.js
@@ -1,1 +1,32 @@
 // Finish the rest of this model
+const { Sequelize, Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/config");
+
+class Post extends Model {}
+
+Post.init(
+    // finish this Comment Model
+ {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    user_id: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: "user",
+            key: "id",
+        },
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    body: {
+      type: DataTypes.STRING,
+    },
+});
+
+module.exports = Post;
