409d968302e74e9d9aff408bcadca3c1d0f5d06d
Candace Robbins <canrob0522@gmail.com>
2022-05-08 15:35:16 -0500

    Udated controller commentRoutes

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -4,6 +4,17 @@ const withAuth = require("../../utils/auth");
 
 router.post("/", withAuth, async (req, res) => {
     // create a router to post comments to created posts
+    try {
+        const commentInfo = await Comment.create(req.body);
+        req.session.save(() => {
+            req.session.user_id = commentInfo.id;
+            req.session.logged_in = true;
+      
+            res.status(200).json(commentInfo);
+          });
+        } catch (err) {
+          res.status(400).json(err);
+        }
 });
 
 module.exports = router;
