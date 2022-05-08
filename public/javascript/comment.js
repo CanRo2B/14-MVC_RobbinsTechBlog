376fbac2d02d10937c7e1ed00737d46d24456f81
Candace Robbins <canrob0522@gmail.com>
2022-05-08 10:42:47 -0500

    Added comment.js

--- public/javascript/comment.js
+++ public/javascript/comment.js
@@ -5,6 +5,20 @@ const commentFormHandler = async function(event) {
     const body = document.querySelector('textarea[name="comment-body"]').value;
   
     // Create the functionality to help create the buttons for your website.
+    if (postID && body) {
+      const response = await fetch("/api/", {
+        method: "POST",
+        body: JSON.stringify({ postId, body }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      if (response.ok) {
+        document.location.resplace("/dashboard");
+      } else {
+        alert("Did not add new blog");
+      }
+    }
   };
   
   document
